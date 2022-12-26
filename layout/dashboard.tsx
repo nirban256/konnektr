eb5ee3fd25fc302fa83c2c26e6f8781ec8d71b55
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2022-12-26 20:02:44 +0530

    fixed padding

--- layout/dashboard.tsx
+++ layout/dashboard.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../components/Core/Sidebar/Sidebar";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen  flex flex-row justify-start px-0 lg:px-0 lg1100:px-0 lg1200:px-0 lg1300:px-[5%] lg1350:px-[10%] xl1500:px-[13%] xl1650:px-[15%] xl1750:px-[17%] xl1950:px-[20%]  bg-white box-border">
+    <div className="h-screen  flex flex-row items-center justify-center px-0  bg-white box-border">
       {true && <Sidebar />}
       <BottomTabBar />
       {children}
